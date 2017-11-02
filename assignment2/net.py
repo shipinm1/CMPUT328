47bab18ce996f8b5cc48c990f794d9cf89649491
shipinm1 <shipin1@ualberta.ca>
2017-11-02 14:26:22 -0600

    last check point loading fixed

--- assignment2/net.py
+++ assignment2/net.py
@@ -1,6 +1,7 @@
 from ops import *
 import timeit
 import time
+from ops import batch_norm
 from cifar10 import Cifar10
 from tensorflow.contrib.layers import flatten
 BATCH_SIZE = 100
@@ -18,28 +19,56 @@ def net(input, is_training, dropout_kept_prob):
   mu = 0
   sigma = 0.1
   
-  
   '''first convolutional layer'''
-  conv1_w = tf.Variable(tf.truncated_normal(shape = (5, 5, 3, 8), mean = mu, stddev = sigma), name = 'conv1_w')
-  conv1_b = tf.Variable(tf.zeros(8), name = 'conv1_b')
-  conv1 = tf.nn.conv2d(input, conv1_w, strides = [1, 1, 1, 1], padding = 'VALID') + conv1_b #(28,28,8)
+  conv1_w = tf.Variable(tf.truncated_normal(shape = (5, 5, 3, 64), mean = mu, stddev = sigma), name = 'conv1_w')
+  conv1_b = tf.Variable(tf.zeros(64), name = 'conv1_b')
+  conv1 = tf.nn.conv2d(input, conv1_w, strides = [1, 1, 1, 1], padding = 'VALID') + conv1_b #(28,28,64)
   #activation function relu
   conv1 = tf.nn.relu(conv1)
   #norm1 = tf.nn.lrn(conv1, 4, bias = 1.0, alpha = 0.001/9, beta = 0.75, name = 'norm1')
   #max pooling
-  conv1 = tf.nn.max_pool(conv1, ksize = [1, 2, 2, 1], strides = [1, 2, 2, 1], padding = 'VALID')#(14,14,8)
+  conv1 = tf.nn.max_pool(conv1, ksize = [1, 2, 2, 1], strides = [1, 2, 2, 1], padding = 'VALID')#(14,14,64)
   print(conv1)
   
   '''second convolutional layer'''
-  conv2_w = tf.Variable(tf.truncated_normal(shape = (5, 5, 8, 16), mean = mu, stddev = sigma), name = 'conv2_w')
-  conv2_b = tf.Variable(tf.zeros(16), name = 'conv2_b')
-  conv2 = tf.nn.conv2d(conv1, conv2_w, strides = [1,1,1,1], padding = 'VALID') + conv2_b #(10,10,16)
+  conv2_w = tf.Variable(tf.truncated_normal(shape = (5, 5, 64, 128), mean = mu, stddev = sigma), name = 'conv2_w')
+  conv2_b = tf.Variable(tf.zeros(128), name = 'conv2_b')
+  conv2 = tf.nn.conv2d(conv1, conv2_w, strides = [1,1,1,1], padding = 'VALID') + conv2_b #(10,10,128)
+  #batch normalization
+  conv2 = batch_norm(conv2, is_training)
   #activation function relu
-  conv2 = tf.nn.relu(conv2) #(10,10,16)
-  conv2 = tf.nn.max_pool(conv2, ksize = [1, 2, 2, 1], strides = [1, 2, 2, 1], padding = 'VALID') #(5, 5, 16)
+  conv2 = tf.nn.relu(conv2) #(10,10,128)
+  conv2 = tf.nn.max_pool(conv2, ksize = [1, 2, 2, 1], strides = [1, 2, 2, 1], padding = 'VALID') #(5, 5, 128)
   print(conv2)
   
-  '''third convolutional layer
+  '''third fully connected layer'''
+  fl1 = flatten(conv2) #(400)
+  print(fl1)
+  fl1_w = tf.Variable(tf.truncated_normal(shape = (3200, 400), mean = mu, stddev = sigma), name = 'fl1_w')
+  fl1_b = tf.Variable(tf.zeros(400), name = 'fl1_b')
+  fl2 = tf.matmul(fl1, fl1_w) + fl1_b
+  #activation function relu
+  fl2 = tf.nn.relu(fl2) #(400)
+  print(fl2)
+  
+  '''forth fully connected layer'''
+  fl2_w = tf.Variable(tf.truncated_normal(shape = (400, 84), mean = mu, stddev = sigma), name = 'fl2_w') 
+  fl2_b = tf.Variable(tf.zeros(84), name = 'fl2_b')
+  fl3 = tf.matmul(fl2, fl2_w) + fl2_b
+  #activation function relu
+  fl3 = tf.nn.relu(fl3)
+  #Dropout
+  fl3_drop = tf.nn.dropout(fl3, dropout_kept_prob)
+  print(fl3)
+  
+  '''fifth fully connected layer'''
+  fl3_w = tf.Variable(tf.truncated_normal(shape = (84, 10), mean = mu, stddev = sigma), name = 'fl3_w')
+  fl3_b = tf.Variable(tf.zeros(10), name = 'fl3_b')
+  flf = tf.matmul(fl3, fl3_w) + fl3_b
+  print(flf)
+  
+  '''
+  #third convolutional layer
   conv3_w = tf.Variable(tf.truncated_normal(shape = (5, 5, 16, 32), mean = mu, stddev = sigma), name = 'conv3_w')
   conv3_b = tf.Variable(tf.zeros(32), name = 'conv2_b')
   conv3 = tf.nn.conv2d(conv2, conv3_w, strides = [1,1,1,1], padding = 'VALID') + conv3_b #(3,3,32)  
@@ -47,33 +76,8 @@ def net(input, is_training, dropout_kept_prob):
   conv3 = tf.nn.relu(conv3)
   #second max pooling
   conv3 = tf.nn.max_pool(conv3, ksize = [1, 2, 2, 1], strides = [1, 2, 2, 1], padding = 'VALID') 
-  #print('this is conv3', conv3)'''
+  #print('this is conv3', conv3)
   
-  '''third fully connected layer'''
-  fl1 = flatten(conv2) #(400)
-  print(fl1)
-  fl1_w = tf.Variable(tf.truncated_normal(shape = (400, 120), mean = mu, stddev = sigma), name = 'fl1_w')
-  fl1_b = tf.Variable(tf.zeros(120), name = 'fl1_b')
-  fl2 = tf.matmul(fl1, fl1_w) + fl1_b
-  #activation function relu
-  fl2 = tf.nn.relu(fl2) #(120)
-  print("fl2", fl2)
-  
-  '''forth fully connected layer'''
-  fl2_w = tf.Variable(tf.truncated_normal(shape = (120, 84), mean = mu, stddev = sigma), name = 'fl2_w') 
-  fl2_b = tf.Variable(tf.zeros(84), name = 'fl2_b')
-  fl3 = tf.matmul(fl2, fl2_w) + fl2_b
-  #activation function relu
-  fl3 = tf.nn.relu(fl3)
-  
-  '''fifth fully connected layer'''
-  fl3_w = tf.Variable(tf.truncated_normal(shape = (84, 10), mean = mu, stddev = sigma), name = 'fl3_w')
-  fl3_b = tf.Variable(tf.zeros(10), name = 'fl3_b')
-  flf = tf.matmul(fl3, fl3_w) + fl3_b
-  return flf
-  
-  
-  '''
   #forth convolutional layer
   conv4_w = tf.Variable(tf.truncated_normal(shape = (3, 3, 32, 64), mean = mu, stddev = sigma), name = 'conv4_w')
   conv4_b = tf.Variable(tf.zeros(64), name = 'conv4_b')
@@ -91,6 +95,10 @@ def net(input, is_training, dropout_kept_prob):
   #activation function relu
   return logits
   '''
+  
+  return flf
+
+
 
 def train():
   # Always use tf.reset_default_graph() to avoid error
@@ -117,7 +125,7 @@ def train():
   
   
   
-  lr = 0.00001
+  lr = 0.000001
   logits = net(x, True, 0.8)
   cross_entropy = tf.nn.softmax_cross_entropy_with_logits(logits = logits, labels = y)
   loss = tf.reduce_mean(cross_entropy)
@@ -140,21 +148,29 @@ def train():
   accuracy_operation = tf.reduce_mean(tf.cast(correct_prediction, tf.float32))
   
   saver = tf.train.Saver(max_to_keep = 5)
+  sess = tf.Session()
+  
+  try:
+    print("================Trying to restore last checkpoint ...================")
+    saver.restore(sess, tf.train.latest_checkpoint('ckpt'))
+    print("================Checkpoint restored .================")
+  except:
+    print("================Failed to find last check point================")
+    sess.run(tf.global_variables_initializer())
   
   
   '''training'''
   start_time = time.time()
-  with tf.Session() as sess:
-    sess.run(tf.global_variables_initializer())
-    #saver.restore(sess, tf.train.latest_checkpoint('ckpt'))
+  with sess:
+    
     print("Training")
     global_step = 0
-    for i in range(100):
+    for i in range(1000):
       for offset in range(0, 500):
         batch_x, batch_y = cifar10_train.get_next_batch()
         _, summaries = sess.run([training_operation, merged_summary_op], feed_dict = {x: batch_x, y: batch_y})
         if global_step % 100 == 1:
-          print("steps: ", global_step, "|time consume: ", (time.time() - start_time)/3600, "hours.")
+          print("steps: ", global_step, "|time consume: %.2f" % ((time.time() - start_time)/3600), "hours.")
           summary_writer.add_summary(summaries, global_step = global_step)
         if global_step % 500 == 0:
           _loss = sess.run(loss, feed_dict = {x:batch_x, y:batch_y})
@@ -206,8 +222,9 @@ def test(cifar10_test_images):
   # DO NOT RUN TRAINING HERE!
   # LOAD THE MODEL AND RUN TEST ON THE TEST SET
   
+  x = tf.placeholder(tf.float32, (None, 32, 32, 3))
   saver = tf.train.Saver()
   with tf.Session() as sess:
     saver.restore(sess, tf.train.latest_checkpoint('ckpt'))
-    test_accuracy = evaluate()
+    test_accuracy = evaluate(cifar10_test_images)
   
